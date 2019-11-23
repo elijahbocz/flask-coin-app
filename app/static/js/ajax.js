658f349f187fb5af9420bff6225ac26f0ad960b4
Unknown <boczelijah@gmail.com>
2019-11-23 16:30:53 -0500

    create event handler for click on history data button, ajax is working just need to parse and display on page

--- app/static/js/ajax.js
+++ app/static/js/ajax.js
@@ -1,22 +1,25 @@
 $('#search').click(function() {
-    var date_input = $('#date_input').val();
-    var coin_name = window.location.pathname.substring(8);
+  var date_input = $('#date_input').val();
+  var coin_name = window.location.pathname.substring(8);
 
-    url = 'https://api.coingecko.com/api/v3/coins/' + coin_name + '/history?date=' + date_input;
+  url =
+    'https://api.coingecko.com/api/v3/coins/' +
+    coin_name +
+    '/history?date=' +
+    date_input;
 
-    var settings = {
-        async: true,
-        crossDomain: true,
-        url: url,
-        method: 'GET',
-        headers: {
-          'cache-control': 'no-cache',
-          'Postman-Token': 'fbc2c5b8-8b4d-4018-901e-1f905b8209cd'
-        }
-      };
-    
-      $.ajax(settings).done(function(response) {
-        console.log(response);
-      });
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: url,
+    method: 'GET',
+    headers: {
+      'cache-control': 'no-cache',
+      'Postman-Token': 'fbc2c5b8-8b4d-4018-901e-1f905b8209cd'
+    }
+  };
 
+  $.ajax(settings).done(function(response) {
+    console.log(response);
+  });
 });
